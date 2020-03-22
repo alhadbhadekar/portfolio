426c702a09e3e88e7a68f3019342189e2af6097b
Alhad Bhadekar <alhad.bhadekar@gmail.com>
2020-03-22 11:53:47 -0400

    Added evens or odds

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,7 +16,7 @@ const Header = ({ children }) => {
             <h3 style = {style}><Link to='/'>Home</Link></h3>
             <h3 style = {style}><Link to='/music-master'>Music Master</Link></h3>
             <h3 style = {style}><Link to='/jokes'>Jokes</Link></h3>
-            {/* <h3 style = {style}><Link to='/evens-or-odds'>Evens or Odds</Link></h3> */}
+            <h3 style = {style}><Link to='/evens-or-odds'>Evens or Odds</Link></h3>
             </div>
             {/* <Component /> */}
             {children}
