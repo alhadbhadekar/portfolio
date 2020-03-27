f0d8ac1035eaab28b2f898f6fe7eba2ddd1c6277
Alhad Bhadekar <alhad.bhadekar@gmail.com>
2020-03-27 19:28:37 -0400

    Modified GitHub link

--- src/data/projects.js
+++ src/data/projects.js
@@ -21,7 +21,7 @@ const PROJECTS = [
         id: 3,
         title: 'GitHub Link for Projects',
         description: 'A GitHub link to my sample projects demonstrating my Software Developement Skills!',
-        link: 'https://github.com/alhadbhadekar?tab=repositories&q=&type=public&language=',
+        link: 'https://github.com/alhadbhadekar?tab=repositories',
         image: project3
     }
 ];
